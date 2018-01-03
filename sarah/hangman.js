be57282974b1f900cc277c5c9d96a6fa1076c62b
root <root@ip-172-31-53-149.ec2.internal>
2018-01-03 05:12:01 +0000

    song no longer on a loop

--- sarah/hangman.js
+++ sarah/hangman.js
@@ -37,6 +37,9 @@
 
 console.log("If you are trying to reverse engineer my code, don't worry, there is nothing malicious...")
 
+// hehe
+new Audio('http://www.soundboard.com/mediafiles/10/101443-244c91f7-5a53-471c-8a0f-8ffa9f316071.mp3').play()
+
 var Hangman = (function () {
     
     'use strict';
@@ -67,7 +70,6 @@ var Hangman = (function () {
         this.showElementByIdWithContent(this.elId + "_foundataion_r", null);
         this.showElementByIdWithContent(this.elId + "_word", this.getGuessedfWord());
 	gtag('event', 'event_name', {'event_category': 'new game', 'event_label': 'click'});
-        new Audio('http://www.soundboard.com/mediafiles/10/101443-244c91f7-5a53-471c-8a0f-8ffa9f316071.mp3').play()        
     };
 
     Hangman.prototype.guess = function (guess) {
